becc41b4e9a41a2d5ae76afb99bd1d183269d300
NEXUS <dpratamesh@gmail.com>
2024-07-01 22:34:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,14 +6,14 @@ function convertToBool(text, fault = 'true') {
 
 
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
-const BOTNAME = 'VAJIRA-MD|-WA-BOT™';
-const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
+const BOTNAME = 'NEXUS-MD|-WA-BOT™';
+const FOOTERNAME = '> NEXUS -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '919945489885'
     
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=rF1UXLSK#_mQAdHYRPwF8-0ERKW4j6AoRGgwvY75jh2pD8iZE9kM' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
-MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
+MODERATORS : process.env.MODERATORS === undefined ? "919945489885" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
 OWNER_NAME: process.env.OWNER_NAME || 'VAJIRA' ,
 COMMAND_TYPE: process.env.COMMAND_TYPE || 'button' ,
